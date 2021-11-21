d647faaef862a7d488c410a01a15103581269d68
Василь Камалов <vasilkamalov@FromKazanWithLove.local>
2021-11-21 11:23:52 +0300

    master release

--- src/components/calc.js
+++ src/components/calc.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-import Button from "./Button";
+import "../styles/app.css";
+
+// Для того чтобы отслеживать нажатия знаков вычисления и реагировать на каждый второй знак
 let counter = 0;
 
 function Calculator() {
@@ -7,8 +9,13 @@ function Calculator() {
     const [namberUn, setNamberUn] = useState("");
 
     const handleNumsAdd = (num) => {
+        if (namberUn.length > 30) {
+            return console.log("Your input number is too long!");
+        }
+
         if (num === '+' || num === '-' || num === '*' || num === '/') {
             counter += 1;
+            console.log("counter: " + counter)
         }
 
         setNamberUn(namberUn + num);
@@ -18,10 +25,11 @@ function Calculator() {
         }
     }
 
+    //Срабатывает при нажатии на равно - делает проверку на знак в строке и отправляет вычисление в ту функцию, которая подходит под этот знак
     const equalNums = () => {
-        let result = 0;
+
         if (typeof namberUn === "number") {
-            return console.log("Please give me more numbers =)")
+            return console.log("Please give me more numbers =)");
         }
 
         const array = [...namberUn];
@@ -31,68 +39,132 @@ function Calculator() {
             if (nums === "+") {
                 plusNums();
             }
+
             if (nums === "-") {
-                minusNums()
+                minusNums();
             }
+
+            if (nums === "*") {
+                multiplication();
+            }
+
+            if (nums === "/") {
+                division();
+            }
+
         });
 
-        function minusNums() {
-            let total = namberUn.split("-");
+        function multiplication() {
+            let result = 1;
+            let total = namberUn.split("*");
+
             total.map((num) => {
                 num = num - 0;
                 if (num === "NaN") {
                     return console.log("NaN")
                 }
-                return result = num - result;
+                return result = num * result;
             })
-            setNamberUn(-result);
+
+            setNamberUn(result);
             counter = 0;
         }
 
+        function division() {
+            let total = namberUn.split("/");
+            let result = (total[0] - 0) / (total[1] - 0)
+            if (!isFinite(result)) {
+                counter = 0;
+                return setNamberUn(0);
+            }
+            counter = 0;
+            setNamberUn(result);
+        }
+
+
+        function minusNums() {
+            counter = 0;
+            let array = [...namberUn];
+
+            let arr = array.slice(1).join("").split("-");
+            let result = (arr[0] - 0) + (arr[1] - 0);
+
+            if (isNaN(result)) {
+                return console.log("isNaN");
+            }
+            setNamberUn(-result);
+        }
+
+
         function plusNums() {
-            let result = 0;
             let total = namberUn.split("+");
 
-            for (let i = 0; i < total.length; i++) {
-                if (isNaN(result)) {
-                    console.log("Nan!!!");
-                }
-                result += total[i]-0;
+            let result = (total[0] - 0) + (total[1] - 0);
+            if (isNaN(result)) {
+                result = (total[1] - 0) + (total[2] - 0);
             }
-
             setNamberUn(result);
             counter = 0;
         }
     }
 
+
     function clearNums() {
-        setNamberUn("")
+        setNamberUn("");
         counter = 0;
     }
 
+    function backspace() {
+        if (typeof namberUn === "number") {
+            var string = namberUn.toString();
+            return setNamberUn(string);
+        }
+        let array = namberUn.split("");
+        array.pop();
+        setNamberUn(array.join(""));
+    }
+
     return (
-        <div className="wrapperCalculator">
+        <div className="calc-wrapper">
             <div>
-                {namberUn}
-            </div>
-            <div>
-                <button onClick={() => handleNumsAdd("1")}>1</button>
-                <button onClick={() => handleNumsAdd("2")}>2</button>
-                <button onClick={() => handleNumsAdd("3")}>3</button>
-                <button onClick={() => handleNumsAdd("4")}>4</button>
-                <button onClick={() => handleNumsAdd("5")}>5</button>
-                <button onClick={() => handleNumsAdd("6")}>6</button>
-                <button onClick={() => handleNumsAdd("7")}>7</button>
-                <button onClick={() => handleNumsAdd("8")}>8</button>
-                <button onClick={() => handleNumsAdd("9")}>9</button>
-                <button onClick={() => handleNumsAdd("0")}>0</button>
-                <button onClick={() => handleNumsAdd("+")}>+</button>
-                <button onClick={() => handleNumsAdd("-")}>-</button>
-                <button onClick={clearNums}>clear</button>
-                <button onClick={() => equalNums("=")}>=</button>
+                <div className="input-col">
+                    <div className="input-col__input">
+                        {namberUn}
+                    </div>
+                    <button className="backspace-button" onClick={backspace}>Backspace</button>
+                </div>
+                <div className="nums-wrapper">
+                    <div className="nums-col">
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("1")}>1</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("2")}>2</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("3")}>3</button>
+                    </div>
+                    <div className="nums-col">
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("4")}>4</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("5")}>5</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("6")}>6</button>
+                    </div>
+                    <div className="nums-col">
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("7")}>7</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("8")}>8</button>
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("9")}>9</button>
+                    </div>
+                    <div className="nums-col">
+                        <button className="nums-col__button" onClick={() => handleNumsAdd("0")}>0</button>
+                        <button className="nums-col__button spec-button" onClick={() => handleNumsAdd("+")}>+</button>
+                        <button className="nums-col__button spec-button" onClick={() => handleNumsAdd("-")}>-</button>
+                    </div>
+                    <div className="nums-col">
+                        <button className="nums-col__button spec-button" onClick={() => handleNumsAdd("*")}>*</button>
+                        <button className="nums-col__button spec-button" onClick={() => handleNumsAdd("/")}>/</button>
+                        <button className="nums-col__button spec-button" onClick={clearNums}>clear</button>
+                    </div>
+                    <div className="nums-col">
+                        <button className="nums-col__last-button" onClick={() => equalNums("=")}>=</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
-
 export default Calculator;
